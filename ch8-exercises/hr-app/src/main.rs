7f514df3086520c4f5935541737415480c328819
Simon Brahan <tntpiranha@gmail.com>
2019-06-26 13:19:20 +0100

    ch8 HR register now parses instruction and can quit

--- ch8-exercises/hr-app/src/main.rs
+++ ch8-exercises/hr-app/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 
 #[derive(Debug)]
@@ -12,9 +13,15 @@ enum Instruction {
 fn main() {
     println!("HR register");
 
+    let mut data = HashMap::new();
+
     loop {
         let instruction = get_instruction();
         println!("{:?}", instruction);
+        if !run_instruction(&mut data, instruction) {
+            println!("Goodbye");
+            break;
+        }
     }
 }
 
@@ -40,15 +47,41 @@ fn get_instruction() -> Instruction {
 
         let instruction_parts: Vec<&str> = instruction_string.split_whitespace().collect();
 
-        if instruction_parts.len() == 2
-            && instruction_parts[0] == "list"
-            && is_department(&instruction_parts[1].to_string())
-        {
+        if instruction_parts.len() == 2 && instruction_parts[0] == "list" {
             return Instruction::ListByDepartment(instruction_parts[1].to_string());
         }
+
+        if instruction_parts.len() == 3
+            && instruction_parts[0] == "add"
+            && instruction_parts[1] == "department"
+        {
+            return Instruction::AddDepartment(instruction_parts[2].to_string());
+        }
+
+        if instruction_parts.len() == 4
+            && instruction_parts[0] == "add"
+            && instruction_parts[2] == "to"
+        {
+            return Instruction::AddStaffMember {
+                name: instruction_parts[1].to_string(),
+                department: instruction_parts[3].to_string(),
+            };
+        }
+
+        println!(
+            "Invalid instruction. Valid instructions:
+                add STAFF NAME to DEPARTMENT NAME
+                add department DEPARTMENT name
+                list all
+                list DEPARTMENT NAME
+                quit"
+        );
     }
 }
 
-fn is_department(department_name: &str) -> bool {
-    true
+fn run_instruction(data: &mut HashMap<String, Vec<String>>, instruction: Instruction) -> bool {
+    match instruction {
+        Instruction::Quit => false,
+        _ => true,
+    }
 }
