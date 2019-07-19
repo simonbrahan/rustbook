b50bd707e809c77e5c2c3996fe8424b0369933c3
Simon Brahan <tntpiranha@gmail.com>
2019-07-19 08:55:57 +0100

    Minigrep reads file, refactored to lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,20 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {} in {}", query, filename);
+    println!("Searching for {} in {}", config.query, config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Problem reading file: {}", e);
+        process::exit(1);
+    }
 }
