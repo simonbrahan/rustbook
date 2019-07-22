7346142e74a44046f30f9da78e3174c7ed9039d2
simon brahan <tntpiranha@gmail.com>
2019-07-22 07:56:33 +0100

    minigrep now prints matches.
    Built and tested search function, integrated with main program.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,7 +23,23 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut output = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            output.push(line);
+        }
+    }
+
+    output
 }
 
 #[cfg(test)]
@@ -54,5 +70,40 @@ mod tests {
             "Two arguments required: search term, file path",
             err.expect_err("Should error because too few args")
         );
+    }
+
+    #[test]
+    fn search_returns_one_result() {
+        let query = "duct";
+        let content = "
+Rust:
+Safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["Safe, fast, productive."], search(query, content));
+    }
+
+    #[test]
+    fn search_returns_many_results() {
+        let query = "duct";
+        let content = "
+Rust:
+Safe, fast, productive.
+Pick three.
+Includes duct tape.";
+
+        assert_eq!(
+            vec!["Safe, fast, productive.", "Includes duct tape."],
+            search(query, content)
+        );
+    }
+
+    #[test]
+    fn search_returns_no_results() {
+        let query = "duct";
+        let content = "Content does not include the search query.";
+
+        let expect: Vec<&str> = vec![];
+        assert_eq!(expect, search(query, content));
     }
 }
