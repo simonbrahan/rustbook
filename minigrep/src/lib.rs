2a8b768e95989340ff06e0723b67f1acd434faae
simon brahan <tntpiranha@gmail.com>
2019-07-22 07:30:21 +0100

    Removed debug statements, added tests for config::new

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -22,7 +23,36 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("File contents:\n{}", contents);
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_build_config_from_args() {
+        let args: Vec<String> = vec![
+            String::from("unused exec path"),
+            String::from("test query"),
+            String::from("path/to/file.txt"),
+        ];
+
+        let conf = Config::new(&args).expect("Should be a Config struct");
+
+        assert_eq!("test query", conf.query);
+        assert_eq!("path/to/file.txt", conf.filename);
+    }
+
+    #[test]
+    fn build_config_fails_with_fewer_args() {
+        let args: Vec<String> = vec![String::from("unused exec path"), String::from("test query")];
+
+        let err = Config::new(&args);
+
+        assert_eq!(
+            "Two arguments required: search term, file path",
+            err.expect_err("Should error because too few args")
+        );
+    }
 }
