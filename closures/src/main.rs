8c6fae978526f83ce1c3f1e9d1c4a667704d140c
Simon Brahan <tntpiranha@gmail.com>
2019-07-23 13:40:48 +0100

    Used cached closure to save expensive calculation

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,12 +1,66 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
-fn main() {
-    println!("Intensity will be {}", simulated_expensive_calculation(2));
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    values: HashMap<u32, u32>,
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("Calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        if !self.values.contains_key(&arg) {
+            self.values.insert(arg, (self.calculation)(arg));
+        }
+
+        *self.values.get(&arg).unwrap()
+    }
+}
+
+fn main() {
+    let user_spec_val = 10;
+    let rand_num = 7;
+
+    generate_workout(user_spec_val, rand_num);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive = Cacher::new(|num| {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive.value(intensity));
+        println!("Next, do {} situps!", expensive.value(intensity));
+    } else if random_number == 3 {
+        println!("Take a break today!");
+    } else {
+        println!("Today, run for {} minutes!", expensive.value(intensity));
+    }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|num| num);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(1, v1);
+    assert_eq!(2, v2);
 }
