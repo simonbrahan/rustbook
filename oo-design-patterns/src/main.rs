5697e8dcca261089b9a028848aee1108a3057735
Simon Brahan <tntpiranha@gmail.com>
2019-08-22 08:47:54 +0100

    First go at oo state pattern.

---
+++ oo-design-patterns/src/main.rs
@@ -0,0 +1,14 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate salad for lunch");
+    assert_eq!("Unpublished draft", post.content());
+
+    post.request_review();
+    assert_eq!("Draft pending review", post.content());
+
+    post.approve();
+    assert_eq!("I ate salad for lunch", post.content());
+}
