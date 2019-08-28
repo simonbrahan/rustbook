f7622681801a84dcf00e489dd7d1a474c76704a0
Simon Brahan <tntpiranha@gmail.com>
2019-08-28 08:57:17 +0100

    Typed blog post drafts can now be rejected.

--- posts-as-types/src/lib.rs
+++ posts-as-types/src/lib.rs
@@ -40,4 +40,10 @@ impl PendingReviewPost {
             content: self.content,
         }
     }
+
+    pub fn reject(self) -> DraftPost {
+        DraftPost {
+            content: self.content,
+        }
+    }
 }
--- posts-as-types/src/main.rs
+++ posts-as-types/src/main.rs
@@ -1,11 +1,17 @@
 use blog::Post;
 
 fn main() {
-    let mut post = Post::new();
+    let mut draft = Post::new();
 
-    post.add_text("I ate salad for lunch today");
+    draft.add_text("I ate salad for lunch today");
 
-    let approved_post = post.request_review().approve();
+    let pending_review = draft.request_review();
+
+    let rejected_draft = pending_review.reject();
+
+    let pending_review = rejected_draft.request_review();
+
+    let approved_post = pending_review.approve();
 
     assert_eq!("I ate salad for lunch today", approved_post.content());
 }
