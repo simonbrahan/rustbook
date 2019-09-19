f8c7e432669ffec06be5020631b8619069e2a6b8
Simon Brahan <tntpiranha@gmail.com>
2019-09-19 08:13:42 +0100

    Webserver prints requests

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,4 +1,17 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    // Clippy suggests read_exact here: however, read_exact always reads enough to fill the buffer.
+    // A request that is smaller than 512 bytes will hang the server
+    // since the stream is never large enough to fill the buffer.
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +19,6 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established");
+        handle_connection(stream);
     }
 }
